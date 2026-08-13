2537b7edd36445fe14774e7d3ab746547d7d2b14
Gajera Prince <gajerap081@gmail.com>
2026-08-13 16:11:11 +0530

    Add comments for test cases in Next_Permutation.cpp
    The code includes comments explaining the input and output for various test cases of the nextPermutation function.

--- 2nd/Next_Permutation.cpp
+++ 2nd/Next_Permutation.cpp
@@ -1,3 +1,16 @@
+// Input: arr[] = [2, 4, 1, 7, 5, 0]
+// Output: [2, 4, 5, 0, 1, 7]
+// Explanation: The next lexicographically greater arrangement of the elements in the array arr[] is [2, 4, 5, 0, 1, 7].
+
+// Input: arr[] = [3, 2, 1]
+// Output: [1, 2, 3]
+// Explanation: This is the last permutation, so we return the lowest possible permutation (ascending order).
+
+// Input: arr[] = [1, 3, 5, 4, 2]
+// Output: [1, 4, 2, 3, 5]
+// Explanation: The next lexicographically greater arrangement of the elements in the array arr[] is [1, 4, 2, 3, 5].
+
+
 #include <iostream> 
 #include <vector> 
 #include <algorithm>
