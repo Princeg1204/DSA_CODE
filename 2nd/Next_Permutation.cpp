7275d541516fd39f2f30f60119fea6b479096165
Gajera Prince <gajerap081@gmail.com>
2026-08-13 16:13:58 +0530

    Refactor nextPermutation function for efficiency

--- 2nd/Next_Permutation.cpp
+++ 2nd/Next_Permutation.cpp
@@ -10,6 +10,59 @@
 // Output: [1, 4, 2, 3, 5]
 // Explanation: The next lexicographically greater arrangement of the elements in the array arr[] is [1, 4, 2, 3, 5].
 
+// [Naive Approach] Generate All Permutations - O(n! * n) Time and O(n! * n) Space
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void generatePermutations(vector<vector<int>> &res, 
+                          vector<int> &arr, int idx) {
+
+    if (idx == arr.size() - 1) {
+        res.push_back(arr);
+        return;
+    }
+    for (int i = idx; i < arr.size(); i++) {
+        swap(arr[idx], arr[i]);
+        generatePermutations(res, arr, idx + 1);
+        swap(arr[idx], arr[i]);
+    }
+}
+void nextPermutation(vector<int>& arr) {
+
+    vector<vector<int>> res;
+    generatePermutations(res, arr, 0);
+    sort(res.begin(), res.end());
+    for (int i = 0; i < res.size(); i++) {
+        if (res[i] == arr) {
+            if (i < res.size() - 1) {
+                arr = res[i + 1];
+            }
+            else {
+                arr = res[0];
+            }
+            break;
+        }
+    }
+}
+
+int main() {
+
+    vector<int> arr = {2, 4, 1, 7, 5, 0};
+
+    nextPermutation(arr);
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+
+// [Expected Approach] Generating Only Next - O(n) Time and O(1) Space
 
 #include <iostream> 
 #include <vector> 
@@ -51,3 +104,5 @@ int main() {
         cout << x << " ";    
     return 0;
 }
+
+
