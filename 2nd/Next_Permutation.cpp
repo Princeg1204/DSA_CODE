e203bcd1ceef8c7a02b46936c637035945681db4
Gajera Prince <gajerap081@gmail.com>
2026-08-13 16:15:24 +0530

    Optimize nextPermutation using STL function
    Refactor nextPermutation function to use inbuilt next_permutation for efficiency.

--- 2nd/Next_Permutation.cpp
+++ 2nd/Next_Permutation.cpp
@@ -105,4 +105,27 @@ int main() {
     return 0;
 }
 
+// [Alternate Approach in C++] Using Inbuilt Function - O(n) Time and O(1) Space
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& arr) {
+    next_permutation(arr.begin(), arr.end());
+}
+
+int main() {
+
+    vector<int> arr = {2, 4, 1, 7, 5, 0};
+
+    nextPermutation(arr);
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
 
