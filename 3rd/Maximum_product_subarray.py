42da4f561545c5d20308d2a77f5689cd8b611d40
Gajera Prince <gajerap081@gmail.com>
2026-08-17 23:02:06 +0530

    Add maxProduct function for maximum product subarray
    Implemented maxProduct function to find the maximum product of a subarray using O(n) time and O(1) space.

--- 3rd/Maximum_product_subarray.py
+++ 3rd/Maximum_product_subarray.py
@@ -29,3 +29,31 @@ if __name__ == "__main__":
     arr = [-2, 6, -3, -10, 0, 2]
     
     print(maxProduct(arr))
+
+# [Expected Approach - 1] Track of Min and Max - O(n) Time and O(1) Space
+
+def maxProduct(arr):
+
+    n = len(arr)
+
+    currMax = arr[0]
+    currMin = arr[0]
+    maxProd = arr[0]
+    for i in range(1, n):
+
+        temp = max(arr[i], arr[i] * currMax, arr[i] * currMin)
+
+        currMin = min(arr[i], arr[i] * currMax, arr[i] * currMin)
+
+        currMax = temp
+
+        maxProd = max(maxProd, currMax)
+
+    return maxProd
+
+
+if __name__ == "__main__":
+    
+    arr = [-2, 6, -3, -10, 0, 2]
+    
+    print(maxProduct(arr))
