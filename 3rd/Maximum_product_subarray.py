8e911f03b0b8157bb17661d93e320c05460bb9e6
Gajera Prince <gajerap081@gmail.com>
2026-08-17 22:10:38 +0530

    Add maxProduct function for maximum product subarray
    Implement naive approach to find maximum product subarray.

---
+++ 3rd/Maximum_product_subarray.py
@@ -0,0 +1,31 @@
+# Input: arr[] = [-2, 6, -3, -10, 0, 2]
+# Output: 180
+# Explanation: The subarray with maximum product is [6, -3, -10] with product = 6 * (-3) * (-10) = 180.
+
+# Input: arr[] = [-1, -3, -10, 0, 6]
+# Output: 30
+# Explanation: The subarray with maximum product is [-3, -10] with product = (-3) * (-10) = 30.
+
+# Input: arr[] = [2, 3, 4] 
+# Output: 24 
+# Explanation: For an array with all positive elements, the result is product of all elements. 
+
+# [Naive Approach] Using Two Nested Loops – O(n^2) Time and O(1) Space
+
+def maxProduct(arr):
+    n = len(arr)
+    maxProd = arr[0]
+
+    for i in range(n):
+        mul = 1
+        for j in range(i, n):
+            mul *= arr[j]
+            maxProd = max(maxProd, mul)
+    
+    return maxProd
+
+if __name__ == "__main__":
+    
+    arr = [-2, 6, -3, -10, 0, 2]
+    
+    print(maxProduct(arr))
