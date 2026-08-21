f7c0f56d44f7c745d60c83fe37f071f023a80206
Gajera Prince <gajerap081@gmail.com>
2026-08-21 09:36:57 +0530

    Enhance LinkedList class with additional methods
    Added print, search, length, and deleteNode methods to LinkedList class.

--- LinkedLists/linkedListClass.cpp
+++ LinkedLists/linkedListClass.cpp
@@ -44,4 +44,93 @@ void LinkedList::insertNodeTail(int val)
     curr->next = new Node(val);
     return;
 }
+void LinkedList::print()
+{
+    // Node *temp = head;
+    // while (temp)
+    // {
+    //     cout << temp->data << "\t";
+    //     temp = temp->next;
+    // }
 
+    forLL(curr)
+    {
+        cout << curr->data << "\t";
+    }
+    cout << "\n";
+}
+
+bool LinkedList::search(int target)
+{
+    forLL(curr)
+    {
+        if (curr->data == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int LinkedList::length()
+{
+    int count = 0;
+    forLL(curr)
+    {
+        count++;
+    }
+    return count;
+}
+
+void LinkedList::deleteNode(int pos)
+{
+    if (!head)
+    {
+        cout << "List is empty, can't delete.\n";
+        return;
+    }
+    if (pos < 1)
+    {
+        cout << "Pos can't be < 1.\n";
+        return;
+    }
+    int _length = length();
+    if (pos > _length)
+    {
+        cout << "Index out of bound.\n";
+        return;
+    }
+    if (pos == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+    Node *curr;
+    for (curr = head; pos - 2; curr = curr->next, pos--)
+    {
+    }
+    Node *temp = curr->next;
+    curr->next = curr->next->next;
+    delete temp;
+}
+
+void solve()
+{
+    LinkedList list1;
+    // list1.insertNodeTail(1);
+    forn(i, 5)
+    {
+        list1.insertNodeTail(i + 1);
+    }
+    list1.deleteNode(5);
+    list1.print();
+    // cout << list1.search(4) << "\n";
+    // cout << list1.length() << "\n";
+}
+
+int main()
+{
+    solve();
+}
