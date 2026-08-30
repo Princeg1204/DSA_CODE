22bfe1c2905c1e1e4eeb0acb83b104ea41a82409
Gajera Prince <gajerap081@gmail.com>
2026-08-30 21:40:01 +0530

    Add maxProduct function for maximum product subarray
    Implemented maxProduct function to calculate maximum product subarray using a two-directional traversal approach.

--- 3rd/Maximum_product_subarray.py
+++ 3rd/Maximum_product_subarray.py
@@ -57,3 +57,30 @@ if __name__ == "__main__":
     arr = [-2, 6, -3, -10, 0, 2]
     
     print(maxProduct(arr))
+
+# [Expected Approach - 2] By Traversing in Both Directions - O(n) Time and O(1) Space
+
+def maxProduct(arr):
+
+    n = len(arr)
+    maxProd = float('-inf')
+    leftToRight = 1
+    rightToLeft = 1
+  
+    for i in range(n):
+        if leftToRight == 0:
+            leftToRight = 1
+        if rightToLeft == 0:
+            rightToLeft = 1
+        leftToRight *= arr[i]
+        j = n - i - 1
+        rightToLeft *= arr[j]
+        maxProd = max(leftToRight, rightToLeft, maxProd)
+    
+    return maxProd
+
+if __name__=="__main__":
+    
+    arr = [-2, 6, -3, -10, 0, 2]
+    
+    print(maxProduct(arr))
