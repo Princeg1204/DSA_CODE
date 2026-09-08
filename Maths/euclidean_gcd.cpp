f0f588f5be024139841614f22580b4ec9a2174a8
Gajera Prince <gajerap081@gmail.com>
2026-09-08 15:20:51 +0530

    Add files via upload

---
+++ Maths/euclidean_gcd.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define pb push_back
+
+vi input_vector()
+{
+    int n;
+    cin >> n;
+    vi v(n);
+    forn(i, n)
+    {
+        cin >> v[i];
+    }
+    return v;
+}
+
+void print_vec(vi &v)
+{
+    forn(i, v.size())
+    {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << lcm(a, b) << "\n";
+}
+
+int main()
+{
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
